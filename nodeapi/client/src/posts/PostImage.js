2681ec68453b6e6dacd4848eaa472de092d7708d
Akshay Prassanna <50097410+TheOmegaWolf@users.noreply.github.com>
2021-05-10 16:40:40 +0530

    Akshay - Image Post Working, Small files

--- nodeapi/client/src/posts/PostImage.js
+++ nodeapi/client/src/posts/PostImage.js
@@ -1,37 +1,67 @@
-import React, { Component } from "react";
-import { Modal, Button } from "react-bootstrap";
-import DragDropImages from "./DragDropImages";
-class PostImage extends Component {
-  state = {
-    showImage: false,
-  };
-  handleClose = () => {
-    this.setState({ showImage: false });
-  };
-  render() {
-    return (
-      <>
-        <Button
-          type="button"
-          onClick={() => {
-            this.setState({ showImage: true });
-            // console.log("showImage:", this.state.showImage);
-          }}
-        >
-          Photo
-        </Button>
-        {/* <div className="h-75 d-inline-block"> */}
-        <Modal show={this.state.showImage} onHide={this.handleClose}>
-          <Modal.Header closeButton />
-          <Modal.Body>
-            <h4 className="text-center mb-3">Lets post pics!</h4>
-            <DragDropImages />
-          </Modal.Body>
-        </Modal>
-        {/* </div> */}
-      </>
-    );
-  }
-}
+import React from "react";
+import { DropzoneDialogBase } from "material-ui-dropzone";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { uploadPicture } from "./apiPosts";
+export default function PostImage() {
+  const [open, setOpen] = React.useState(false);
+  const [fileObjects, setFileObjects] = React.useState([]);
 
-export default PostImage;
+  const dialogTitle = () => (
+    <>
+      <span>Upload file</span>
+      <IconButton
+        style={{ right: "12px", top: "8px", position: "absolute" }}
+        onClick={() => setOpen(false)}
+      >
+        <CloseIcon />
+      </IconButton>
+    </>
+  );
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+        Add Image
+      </Button>
+
+      <DropzoneDialogBase
+        dialogTitle={dialogTitle()}
+        acceptedFiles={["image/*"]}
+        fileObjects={fileObjects}
+        cancelButtonText={"cancel"}
+        submitButtonText={"submit"}
+        maxFileSize={10485760}
+        open={open}
+        onAdd={(newFileObjs) => {
+          console.log("onAdd", newFileObjs);
+          setFileObjects([].concat(fileObjects, newFileObjs));
+        }}
+        onDelete={(deleteFileObj) => {
+          let arr = [];
+          fileObjects.map((file) => {
+            // console.log(file.file.name, deleteFileObj.file.name);
+            if (file.file.name !== deleteFileObj.file.name) {
+              arr.push(file);
+            }
+            setFileObjects(arr);
+          });
+          console.log("onDelete", deleteFileObj);
+        }}
+        onClose={() => setOpen(false)}
+        onSave={() => {
+          console.log("onSave", fileObjects);
+          fileObjects.map((file) => {
+            console.log("file data (base64):", file.data);
+            uploadPicture(file.data, file.file.name).then((data) =>
+              console.log("data:", data)
+            );
+          });
+          setOpen(false);
+        }}
+        showPreviews={true}
+        showFileNamesInPreview={true}
+      />
+    </div>
+  );
+}
