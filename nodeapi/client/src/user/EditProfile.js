001a258fdac0faadcbd8f5f29940a0b03e7f14de
Nithiya Soundari K <53947738+Nithiya99@users.noreply.github.com>
2021-06-01 11:12:18 +0800

    Nithiya - Edit Profile Small Update

--- nodeapi/client/src/user/EditProfile.js
+++ nodeapi/client/src/user/EditProfile.js
@@ -144,190 +144,251 @@ class EditProfile extends Component {
         >
           {error}
         </div>
-        <div>
-          <Modal show={open} onHide={this.handleChange}>
-            <Modal.Header>
-              <Modal.Title>Let's change your Display Picture</Modal.Title>
-            </Modal.Header>
+        <div className="card card-custom gutter-b">
+          <div className="card-body">
+            <div className="d-flex">
+              <div className="flex-grow-1">
+                <div className="d-flex align-items-center justify-content-between flex-wrap">
+                  <div className="mr-3">
+                    <Modal show={open} onHide={this.handleChange}>
+                      <Modal.Header>
+                        <Modal.Title>
+                          Let's change your Display Picture
+                        </Modal.Title>
+                      </Modal.Header>
 
-            <Modal.Body>
-              {image !== undefined ? (
-                <img src={URL.createObjectURL(image)} width={"450px"} />
-              ) : (
-                <DragDropProfilePic setImage={this.setImage} />
-              )}
-            </Modal.Body>
+                      <Modal.Body>
+                        {image !== undefined ? (
+                          <img
+                            src={URL.createObjectURL(image)}
+                            width={"450px"}
+                          />
+                        ) : (
+                          <DragDropProfilePic setImage={this.setImage} />
+                        )}
+                      </Modal.Body>
 
-            <Modal.Footer>
-              <Button variant="secondary" onClick={() => this.handleClose}>
-                Close
-              </Button>
-              <Button
-                variant="primary"
-                onClick={() => {
-                  if (image !== undefined) {
-                    postProfilePic(image).then((url) => {
-                      if (url) {
-                        toast.success("profile picture updated");
-                        this.props.setProfilePic({ profilePic: url });
-                        this.handleClose();
+                      <Modal.Footer>
+                        <Button
+                          variant="secondary"
+                          onClick={() => this.handleClose}
+                        >
+                          Close
+                        </Button>
+                        <Button
+                          variant="primary"
+                          onClick={() => {
+                            if (image !== undefined) {
+                              postProfilePic(image).then((url) => {
+                                if (url) {
+                                  toast.success("profile picture updated");
+                                  this.props.setProfilePic({ profilePic: url });
+                                  this.handleClose();
+                                }
+                              });
+                            }
+                          }}
+                        >
+                          Save changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                    <img
+                      src={dp}
+                      alt={initialName}
+                      className="symbol symbol-60 symbol-xxl-100 mr-3 align-self-start align-self-xxl-center"
+                      style={{ width: "55px" }}
+                    />
+                    <button
+                      className="btn btn-light-warning btn-sm font-weight-bold btn-upper btn-text m-1"
+                      onClick={() => {
+                        this.handleOpen();
+                      }}
+                    >
+                      Edit DP
+                    </button>
+                    <button
+                      className="btn btn-light-danger btn-sm font-weight-bold btn-upper btn-text m-1"
+                      onClick={() =>
+                        removeProfilePic().then((data) => {
+                          console.log("dp:", data);
+                        })
                       }
-                    });
-                  }
-                }}
-              >
-                Save changes
-              </Button>
-            </Modal.Footer>
-          </Modal>
-          <img
-            src={dp}
-            alt={initialName}
-            className="symbol symbol-60 symbol-xxl-100 mr-3 align-self-start align-self-xxl-center"
-            style={{ width: "55px" }}
-          />
-          <Button
-            onClick={() => {
-              this.handleOpen();
-            }}
-          >
-            Edit DP
-          </Button>
-          <Button
-            onClick={() =>
-              removeProfilePic().then((data) => {
-                console.log("dp:", data);
-              })
-            }
-          >
-            Delete DP
-          </Button>
+                    >
+                      Delete DP
+                    </button>
+                  </div>
+                  <h3>hello</h3>
+                </div>
+                <form className="mt-5">
+                  <h3 className="pd-5">Personal Info</h3>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Username</label>
+                        <input
+                          onChange={this.handleChange("username")}
+                          type="text"
+                          className="form-control"
+                          value={username}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Name</label>
+                        <input
+                          onChange={this.handleChange("name")}
+                          type="text"
+                          className="form-control"
+                          value={name}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Email</label>
+                        <input
+                          onChange={this.handleChange("email")}
+                          type="email"
+                          className="form-control"
+                          value={email}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Password</label>
+                        <input
+                          onChange={this.handleChange("password")}
+                          type="password"
+                          className="form-control"
+                          value={password}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Bio</label>
+                        <input
+                          onChange={this.handleChange("bio")}
+                          type="text"
+                          className="form-control"
+                          value={bio}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Location</label>
+                        <input
+                          onChange={this.handleChange("location")}
+                          type="text"
+                          className="form-control"
+                          value={location}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <SkillsInput
+                          label={"Skills"}
+                          setSkills={this.handleSkills}
+                          value={skillstr}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <h3>Social Info</h3>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Instagram</label>
+                        <input
+                          onChange={this.handleSocialChange("instagram")}
+                          type="text"
+                          className="form-control"
+                          value={social.instagram}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Facebook</label>
+                        <input
+                          onChange={this.handleSocialChange("facebook")}
+                          type="text"
+                          className="form-control"
+                          value={social.instagram}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Your Website</label>
+                        <input
+                          onChange={this.handleSocialChange("website")}
+                          type="text"
+                          className="form-control"
+                          value={social.website}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Linkedin</label>
+                        <input
+                          onChange={this.handleSocialChange("linkedin")}
+                          type="text"
+                          className="form-control"
+                          value={social.linkedin}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Youtube</label>
+                        <input
+                          onChange={this.handleSocialChange("youtube")}
+                          type="text"
+                          className="form-control"
+                          value={social.youtube}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        <label>Twitter</label>
+                        <input
+                          onChange={this.handleSocialChange("twitter")}
+                          type="text"
+                          className="form-control"
+                          value={social.twitter}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    onClick={this.clickSubmit}
+                    className="btn btn-raised btn-primary"
+                  >
+                    Update
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        <form>
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              onChange={this.handleChange("username")}
-              type="text"
-              className="form-control"
-              value={username}
-            />
-          </div>
-          <div className="form-group">
-            <label>Name</label>
-            <input
-              onChange={this.handleChange("name")}
-              type="text"
-              className="form-control"
-              value={name}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              onChange={this.handleChange("email")}
-              type="email"
-              className="form-control"
-              value={email}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              onChange={this.handleChange("password")}
-              type="password"
-              className="form-control"
-              value={password}
-            />
-          </div>
-          <div className="form-group">
-            <label>Bio</label>
-            <input
-              onChange={this.handleChange("bio")}
-              type="text"
-              className="form-control"
-              value={bio}
-            />
-          </div>
-          <div className="form-group">
-            <label>Location</label>
-            <input
-              onChange={this.handleChange("location")}
-              type="text"
-              className="form-control"
-              value={location}
-            />
-          </div>
-          <div className="form-group">
-            <SkillsInput
-              label={"Skills"}
-              setSkills={this.handleSkills}
-              value={skillstr}
-            />
-          </div>
-          <div className="form-group">
-            <label>Instagram</label>
-            <input
-              onChange={this.handleSocialChange("instagram")}
-              type="text"
-              className="form-control"
-              value={social.instagram}
-            />
-          </div>
-          <div className="form-group">
-            <label>Facebook</label>
-            <input
-              onChange={this.handleSocialChange("facebook")}
-              type="text"
-              className="form-control"
-              value={social.instagram}
-            />
-          </div>
-          <div className="form-group">
-            <label>Your Website</label>
-            <input
-              onChange={this.handleSocialChange("website")}
-              type="text"
-              className="form-control"
-              value={social.website}
-            />
-          </div>
-          <div className="form-group">
-            <label>Linkedin</label>
-            <input
-              onChange={this.handleSocialChange("linkedin")}
-              type="text"
-              className="form-control"
-              value={social.linkedin}
-            />
-          </div>
-          <div className="form-group">
-            <label>Youtube</label>
-            <input
-              onChange={this.handleSocialChange("youtube")}
-              type="text"
-              className="form-control"
-              value={social.youtube}
-            />
-          </div>
-          <div className="form-group">
-            <label>Twitter</label>
-            <input
-              onChange={this.handleSocialChange("twitter")}
-              type="text"
-              className="form-control"
-              value={social.twitter}
-            />
-          </div>
-          <button
-            onClick={this.clickSubmit}
-            className="btn btn-raised btn-primary"
-          >
-            Update
-          </button>
-        </form>
       </div>
     );
   }
